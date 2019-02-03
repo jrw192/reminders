f35687d6ca709221ec1889ee7e0085e3df2c27bc
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-02-03 02:56:30 -0600

    background text

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 onClick = (event) => {
-	console.log("that's some cheese");
 	var phrase = text[Math.floor(Math.random()*text.length)];
-	console.log("phrase: ", phrase, " curPhrase: ", curPhrase);
 	while(phrase === curPhrase) {
 		phrase = text[Math.floor(Math.random()*text.length)]; //no repeats
-		console.log("new phrase: ", phrase);
 	}
 	curPhrase = phrase;
-
+	smols = [];
 	while(scene.children.length > 0){ 
-		console.log("children");
 	    scene.remove(scene.children[0]); 
 	}
 	makeText(phrase);
+
+	makeAllMinor();
 	render();
 }
 
@@ -28,19 +26,13 @@ var scene = new THREE.Scene();
 
 // Create a basic perspective camera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 100;
+camera.position.z = 120;
 
 
 var controls = new THREE.OrbitControls( camera );
 
 //light
 var light = new THREE.AmbientLight( 0x404040, 5 ); // soft white light
-
-
-// var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
-// directionalLight.position.set( - 1, 1, 1 );
-// scene.add( directionalLight );
-
 // Create a renderer with Antialiasing
 var renderer = new THREE.WebGLRenderer({antialias:true});
 
@@ -60,7 +52,7 @@ var group = new THREE.Group();
 scene.add(group);
 
 var text;
-var curPhrase;
+var curPhrase = "";
 var meshes = [];
 
 
@@ -81,17 +73,17 @@ function readTextFile(file)
     }
     rawFile.send(null);
 }
-
 readTextFile('words.txt');
 
 function makeText(phrase) {
+	console.log("boop")
 	loader.load( './ms_yahei_regular.json', function ( font ) {
 
 		var geometry = new THREE.TextGeometry( phrase, {
 			font: font,
 			size: 20,
 			height: 2,
-			curveSegments: 12,
+			curveSegments: 5,
 			bevelEnabled: true,
 			bevelThickness: 1,
 			bevelSize: 1,
@@ -99,7 +91,8 @@ function makeText(phrase) {
 	} );
 
 	//var material = new THREE.MeshBasicMaterial({ color: 'pink'} );
-	var material = new THREE.MeshLambertMaterial({color: 'white', transparent: true, opacity: 0.7});
+	// var material = new THREE.MeshLambertMaterial({color: 'white', transparent: true, opacity: 0.7});
+	var material = new THREE.MeshLambertMaterial({color: 'white', transparent: false});
 	var mesh = new THREE.Mesh(geometry, material);
 	var edges = new THREE.EdgesHelper(mesh, 'grey');
 	mesh.add(edges);
@@ -112,17 +105,116 @@ function makeText(phrase) {
 }
 
 
+function makeMinorText(phrase, x, y, z) {
+	var mesh;
+	var direction;
+
+	this.init = () => {
+		loader.load( './ms_yahei_regular.json', function ( font ) {
+
+			var geometry = new THREE.TextGeometry( phrase, {
+				font: font,
+				size: 1,
+				height: 0,
+				curveSegments: 5,
+		} );
+
+		//var material = new THREE.MeshBasicMaterial({ color: 'pink'} );
+		// var material = new THREE.MeshLambertMaterial({color: 'white', transparent: true, opacity: 0.7});
+		var material = new THREE.MeshLambertMaterial({color: 'white', transparent: false});
+		var mesh = new THREE.Mesh(geometry, material);
+		//var edges = new THREE.EdgesHelper(mesh, 'grey');
+		//mesh.add(edges);
+		mesh.position.set(x, y, z);
+		scene.add(mesh);
+		
+		});
+		direction = new THREE.Vector3(Math.random(), Math.random(), 0);
+	}
+	this.init();
+
+	this.move = () => {
+		let x = mesh.position.x,
+			y = mesh.position.y;
+		let dX = direction.x,
+			dY = direction.y;
+
+		var magnitude = Math.sqrt( (dX*dX) + (dY*dY) );
+		newX = x + dX * 1.3 / magnitude;
+		newY = y + dY * 1.3 / magnitude;
+
+		mesh.position.set(newX, newY, z);
+
+		var frustum = new THREE.Frustum();
+		frustum.setFromMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse)); 
+
+		//change fish direction when fish reaches edge of pond
+		var pos = new THREE.Vector3(mesh.position.x + 5, mesh.position.y + 5, 0);
+		if (Math.abs(pos.x) > 200) {
+			console.log("x limit");
+			let randX = Math.random() * 0.4,
+				randY = Math.random() * 3;
+			randX = [-randX, randX][Math.floor(Math.random()) * 2];
+			randY = [-randY, randY][Math.floor(Math.random()) * 2];
+			console.log(randX,randY);
+
+
+			direction.x = -direction.x + randX;
+			//direction.y = direction.y + randY;
+
+		}
+		if (Math.abs(pos.y) > 200) {
+			console.log("y limit");
+			let randX = Math.random() * 3,
+				randY = Math.random() * 0.4;
+			randX = [-randX, randX][Math.floor(Math.random()) * 2];
+			randY = [-randY, randY][Math.floor(Math.random()) * 2];
+			console.log(randX,randY);
+
+			//direction.x = direction.x + randX;
+			direction.y = -direction.y + randY;
+		}
+
+	}
+}
+
+var xVals = [];
+var yVals = [];
+var smols = [];
+
+for(i= 0-text.length; i<text.length;i++) {
+	xVals.push(i*12);
+	yVals.push(i*11);
+}
+
+console.log(xVals);
+function makeAllMinor() {
+	let xTemp = xVals;
+	let yTemp = yVals;
+	for(i = 0; i < text.length; i++) {
+		if(text[i] !== curPhrase) {
+			let x = xTemp[Math.floor(Math.random() * xTemp.length)];
+			let y = yTemp[Math.floor(Math.random() * yTemp.length)];
+			// let xInd = xTemp.indexOf(x),
+			// 	yInd = yTemp.indexOf(y);
+			// if(xInd > -1)
+			// 	xTemp.splice(xInd, 1);
+			// if(yInd > -1)
+			// 	yTemp.splice(yInd, 1);
+			smols[i] = new makeMinorText(text[i], x, y, -10);
+		}
+	}
+}
 
 function render() {
 	requestAnimationFrame(render);
 	renderer.render( scene, camera );
-
 }
-
 
 function init() {
 	curPhrase = text[Math.floor(Math.random()*text.length)];
 	makeText(curPhrase);
+	makeAllMinor();
 	render();
 	console.log("hello")
 }
